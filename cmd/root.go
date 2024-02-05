ce320b234752d3882d7481d16e4627c927f95536
Richard Curteis <richard@realizesec.com>
2024-02-05 22:33:41 +0000

    Move main to cmd/orbit and refactor CLI args

--- cmd/root.go
+++ cmd/root.go
@@ -17,9 +17,11 @@ func Execute() error {
 }
 
 func init() {
-	RootCmd.PersistentFlags().StringP("file", "f", "", "Input file path.")
-	RootCmd.PersistentFlags().BoolP("a-records", "a", false, "Print A and AAA records.")
-	RootCmd.PersistentFlags().BoolP("cnames", "c", false, "Print CNAME records.")
-	RootCmd.PersistentFlags().BoolP("targets", "t", false, "Created an FQDN target list. Uses CNAME and A/AAA.")
-	RootCmd.PersistentFlags().BoolP("ips", "i", false, "Created target list. Uses IPv4 and IPv6 values.")
+	RootCmd.PersistentFlags().String("iZ", "", "Input is .zone file or directory.")
+	RootCmd.PersistentFlags().String("iI", "", "Input file containing IP addresses.")
+	RootCmd.PersistentFlags().String("iU", "", "Input file containing URLs.")
+	//RootCmd.PersistentFlags().BoolP("a-records", "a", false, "Print A and AAA records.")
+	//RootCmd.PersistentFlags().BoolP("cnames", "c", false, "Print CNAME records.")
+	//RootCmd.PersistentFlags().BoolP("targets", "t", false, "Created an FQDN target list. Uses CNAME and A/AAA.")
+	//RootCmd.PersistentFlags().BoolP("ips", "i", false, "Created target list. Uses IPv4 and IPv6 values.")
 }
