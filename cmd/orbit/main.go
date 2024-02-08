3c35ed86b613ed36302100164a55d1dc17ea7b16
Richard Curteis <richard@realizesec.com>
2024-02-08 23:16:28 +0000

    Refactor analysis logic

--- cmd/orbit/main.go
+++ cmd/orbit/main.go
@@ -17,10 +17,11 @@ import (
 )
 
 var (
-	zones = zone_files.DNSZones{}
-	ipa   = ip_addresses.IPAddresses{}
-	rep   = reporting.Reporting{}
-	dna   = dns_analysers.DNSAnalyser{}
+	zones  = zone_files.DNSZones{}
+	ipa    = ip_addresses.IPAddresses{}
+	rep    = reporting.Reporting{}
+	dna    = dns_analysers.DNSAnalyser{}
+	assess = models.ASMAssessment{}
 )
 
 func main() {
@@ -31,48 +32,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	var assess = models.ASMAssessment{}
-
 	zf, _ := cmd.RootCmd.PersistentFlags().GetString("iZ")
 	if zf != "" {
-		zfResults, err := zones.GetZoneData(zf)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		assess.Zones = zfResults
+		getZoneData(zf)
 	}
 
 	ips, _ := cmd.RootCmd.PersistentFlags().GetString("iI")
 	if ips != "" {
-		existingIps, err := file_management.ReadFileLines(ips)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		var ipMod models.IPCollection
-		for i := range existingIps {
-			if ip := net.ParseIP(existingIps[i]); ip != nil {
-				if ipa.IsIPv4(ip) {
-					ipMod.IPv4 = append(ipMod.IPv4, ip)
-				} else {
-					ipMod.IPv6 = append(ipMod.IPv6, ip)
-				}
-			}
-		}
-		assess.IPAddresses = ipMod
+		readIPsFile(ips)
 	}
 
 	urlList, _ := cmd.RootCmd.PersistentFlags().GetString("iU")
 	if urlList != "" {
-		existingUrls, err := file_management.ReadFileLines(urlList)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		for i := range existingUrls {
-			assess.Domains = append(assess.Domains, existingUrls[i])
-		}
+		readUrlsFile(urlList)
 	}
 
 	// Get IP addresses from A/AAA records
@@ -82,6 +54,59 @@ func main() {
 	}
 
 	// Get aliases
+	getAliasesFromZones()
+
+	// Lookup IPs of all known domains and track otherwise unknown IPs
+	domainIPLookups()
+
+	processReverseLookups()
+
+	// Review all IP addresses and note any exposed internal IP addresses in records.
+	reviewPrivateIPs()
+
+	printURLTargets()
+}
+
+func getZoneData(zf string) {
+	zfResults, err := zones.GetZoneData(zf)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	assess.Zones = zfResults
+}
+
+func readIPsFile(ips string) {
+	existingIps, err := file_management.ReadFileLines(ips)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	var ipMod models.IPCollection
+	for i := range existingIps {
+		if ip := net.ParseIP(existingIps[i]); ip != nil {
+			if ipa.IsIPv4(ip) {
+				ipMod.IPv4 = append(ipMod.IPv4, ip)
+			} else {
+				ipMod.IPv6 = append(ipMod.IPv6, ip)
+			}
+		}
+	}
+	assess.IPAddresses = ipMod
+}
+
+func readUrlsFile(urlList string) {
+	existingUrls, err := file_management.ReadFileLines(urlList)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for i := range existingUrls {
+		assess.Domains = append(assess.Domains, existingUrls[i])
+	}
+}
+
+func getAliasesFromZones() {
 	for _, zone := range assess.Zones {
 		aliases := rep.CNAMERecords(&zone)
 		// Remove .gtm domains. These are mostly subdomains used elsewhere
@@ -109,31 +134,25 @@ func main() {
 			assess.Aliases = append(assess.Aliases, al)
 		}
 
-		o := zone.Origin
 		// Check for DNSSEC enablement
-		if res, _ := dna.DNSSECEnabled(o); res {
-			rep.AddMissingDNSSec(o, &assess)
+		if res, _ := dna.DNSSECEnabled(zone.Origin); res {
+			rep.AddMissingDNSSec(zone.Origin, &assess)
 		}
-		ip := net.ParseIP(o)
-		if ip != nil {
-			if !ipa.IPExistsIn(ip, &assess.IPAddresses) {
-				ipa.CheckAddIPtoAddresses(ip, &assess.IPAddresses)
-			}
+
+		ip := net.ParseIP(zone.Origin)
+		if ip != nil && !ipa.IPExistsIn(ip, &assess.IPAddresses) {
+			ipa.CheckAddIPtoAddresses(ip, &assess.IPAddresses)
 		} else {
-			targets := rep.GetFQDNs(&zone)
-			for i := range targets {
-				rep.AddURLToAsmDomainsDupSafe(targets[i], &assess)
+			fqdns := rep.GetFQDNs(&zone)
+			for i := range fqdns {
+				rep.AddURLToAsmDomainsDupSafe(fqdns[i], &assess)
 			}
 		}
 	}
+}
 
-	// Sanity check A/AAA against domains
-	//for _, zone := range assess.Zones {
-	//	res, _ := dna.GetCNAME(zone.Origin)
-	//	fmt.Println(res)
-	//}
-
-	// Reverse lookups
+func processReverseLookups() {
+	allowed := []string{"money", "fx", "ttt", "novo", "explore", "currency"}
 	for _, ip := range assess.IPAddresses.IPv4 {
 		domains, err := dna.ReverseLookup(ip.String())
 		if err != nil {
@@ -141,7 +160,6 @@ func main() {
 		}
 		for _, d := range domains {
 			// Filter out keywords specific to target - Needed to avoid enumerating all third-party services using public cloud load balancers
-			allowed := []string{"money", "fx", "ttt", "novo", "explore", "currency"}
 			hasSub := func(allowed []string, domain string) bool {
 				for i := range allowed {
 					if strings.Contains(domain, allowed[i]) {
@@ -150,35 +168,31 @@ func main() {
 				}
 				return false
 			}
-			if hasSub(allowed, d) {
+			isTracked := false
+			if !hasSub(allowed, d) {
+				break
+			}
+			if rep.SliceContainsString(assess.Domains, d) {
+				isTracked = true
+			}
 
-				isTracked := false
-
-				for i := range assess.Domains {
-					if d == assess.Domains[i] {
-						isTracked = true
-						break
-					}
-				}
-
-				if !isTracked {
-					if len(assess.UntrackedDomains) == 0 {
+			if isTracked {
+				break
+			}
+			if len(assess.UntrackedDomains) == 0 {
+				// Duplicated code
+				inner := map[string][]string{d: {ip.String()}}
+				assess.UntrackedDomains = append(assess.UntrackedDomains, inner)
+			} else {
+				for _, ut := range assess.UntrackedDomains {
+					utDomains := ut[d]
+					if _, exists := ut[d]; !exists {
 						// Duplicated code
 						inner := map[string][]string{d: {ip.String()}}
 						assess.UntrackedDomains = append(assess.UntrackedDomains, inner)
 					} else {
-						for _, ut := range assess.UntrackedDomains {
-							utDomains := ut[d]
-							if _, exists := ut[d]; !exists {
-								// Duplicated code
-								inner := map[string][]string{d: {ip.String()}}
-								assess.UntrackedDomains = append(assess.UntrackedDomains, inner)
-							} else {
-								if !rep.SliceContainsString(utDomains, ip.String()) {
-									utDomains = append(utDomains, ip.String())
-								}
-							}
-
+						if !rep.SliceContainsString(utDomains, ip.String()) {
+							utDomains = append(utDomains, ip.String())
 						}
 					}
 
@@ -186,8 +200,9 @@ func main() {
 			}
 		}
 	}
+}
 
-	// Lookup IPs of all known domains and track otherwise unknown IPs
+func domainIPLookups() {
 	for i := range assess.Domains {
 		ips, err := dna.IPLookup(assess.Domains[i])
 		if err != nil {
@@ -201,8 +216,9 @@ func main() {
 			}
 		}
 	}
+}
 
-	// Review all IP addresses and note any exposed internal IP addresses in records.
+func reviewPrivateIPs() {
 	tracked := append(assess.IPAddresses.IPv4, assess.IPAddresses.IPv6...)
 	untracked := append(assess.UntrackedIPAddresses.IPv4, assess.UntrackedIPAddresses.IPv6...)
 	allIps := append(tracked, untracked...)
@@ -212,7 +228,9 @@ func main() {
 			ipa.NoCheckAddIPtoAddresses(allIps[i], &assess.PrivateIPAddresses)
 		}
 	}
+}
 
+func printURLTargets() {
 	targets := make([]string, len(assess.Domains))
 
 	// Print web targets
